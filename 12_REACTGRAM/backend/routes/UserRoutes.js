fa4baf8eaa892fc40207b7a284c98a0d5a2669e1
DiegoCRM <dcrmendes26@gmail.com>
2023-04-19 13:12:16 -0300

    ReactGram
    inicio do back-end no projeto

---
+++ 12_REACTGRAM/backend/routes/UserRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const router = express.Router()
+
+//controller
+const {
+  register,
+  login,
+  getCurrentUser,
+  update
+} = require("../controllers/UserController");
+
+// Middlewares
+const validate = require("../middlewares/handleValidation");
+const {
+  userCreateValidation,
+  loginValidation,
+  userUpdatevalidation
+} = require("../middlewares/userValidations");
+const authGuard = require("../middlewares/authGuard");
+const { imageUpload } = require("../middlewares/imageUpload");
+
+//Routes
+router.post("/register", userCreateValidation(), validate, register);
+router.post("/login", loginValidation(), validate, login);
+router.get("/profile", authGuard, getCurrentUser);
+router.put("/", authGuard, userUpdatevalidation(), validate, imageUpload.single("profileImage"), update);
+
+module.exports = router;
