cfe49d8e7230c2aa6bb5d73471843a10e465a2ee
DiegoCRM <dcrmendes26@gmail.com>
2023-08-16 13:48:40 -0300

    Iniciando o curso
    Atualizando com as novas funções do curso de React

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -0,0 +1,18 @@
+//styles
+import "./App.css";
+
+//components
+import FirstComponent from "./components/FirstComponent";
+
+function App() {
+  return (
+    <>
+      <h1>Fundamentos React</h1>
+
+      <FirstComponent/>
+
+    </>
+  );
+}
+
+export default App;
